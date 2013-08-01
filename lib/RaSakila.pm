6ce6787867bedadcd848fbc638b4b55549fbf007
Henry Van Styn <vanstyn@intellitree.com>
2013-08-01 14:35:08 -0400

    Removed Hp3k/Greensheet sources + stipped down cnf

--- lib/RaSakila.pm
+++ lib/RaSakila.pm
@@ -1,82 +1,41 @@
 package RaSakila;
-#use Moose;
-#use namespace::autoclean;
+use Moose;
+use namespace::autoclean;
 
+use Catalyst;
 use Catalyst::Runtime 5.80;
-
-# Set flags and add plugins for the application.
-#
-# Note that ORDERING IS IMPORTANT here as plugins are initialized in order,
-# therefore you almost certainly want to keep ConfigLoader at the head of the
-# list if you're using it.
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
-# Find the rapidapp lib directory. Assume we're being ran from script/ dir
-# of a standard Catalyst App and the rapidapp repo is at rapidapp/
-use FindBin;
-use lib "$FindBin::Bin/../rapidapp/lib";
-
-use RapidApp::Include qw(sugar perlutil);
-
+use RapidApp 0.99014;
 
 my @plugins = qw(
     -Debug
-    Static::Simple
     RapidApp::RapidDbic
 );
-
 push @plugins, qw(RapidApp::AuthCore);
 push @plugins, qw(RapidApp::NavCore);
-#push @plugins, qw(RapidApp::RequestLogger);
 
-use Catalyst;
-
-#extends 'RapidApp::Catalyst';
-
-#extends 'Catalyst';
-#with 'Catalyst::Plugin::RapidApp';
-
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our $TITLE = "RapidApp Demo1 v" . $RaSakila::VERSION;
-
-# Configure the application.
-#
-# Note that settings in rasakila.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
-
 
 __PACKAGE__->config(
   name => 'RaDemo1',
   # Disable deprecated behavior needed by old applications
   disable_component_resolution_regex_fallback => 1,
-  'Model::RapidApp::CoreSchema' => {
-    #db_file => 'foo.db'
-  },
-  'Plugin::RapidApp::NavCore' => {
-    allow_manage => 1, #<-- default '1'
-    user_views => 1 #<-- default '1'
-  },
+
   'Plugin::RapidApp::RapidDbic' => {
     title => $TITLE,
     nav_title => 'Sample Databases',
-    dashboard_template => 'templates/dashboard.tt',
-    page_view_dir => 'root/pages',
+    
     dbic_models => [
       'Sakila',
       'MixedArticles',
       'Countries',
-      'RapidApp::CoreSchema',
       'ItsInventory'
     ],
+    
+}); __PACKAGE__->setup(@plugins); __END__
+
+    dashboard_template => 'templates/dashboard.tt',
+    
     hide_fk_columns => 1,
     configs => {
       'RapidApp::CoreSchema' => {
@@ -267,33 +226,5 @@ __PACKAGE__->config(
 
 # Start the application
 __PACKAGE__->setup(@plugins);
-
-
-=head1 NAME
-
-RaSakila - Catalyst based application
-
-=head1 SYNOPSIS
-
-    script/rasakila_server.pl
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 SEE ALSO
-
-L<RaSakila::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-root
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
